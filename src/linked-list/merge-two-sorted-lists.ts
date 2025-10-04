c322031f76605e66f9d216a0c785df92a19ab8d9
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-10-04 09:49:46 -0300

    feat: complete algoritms to detect looping on linked list

---
+++ src/linked-list/merge-two-sorted-lists.ts
@@ -0,0 +1,28 @@
+import type { ListNode } from "./list-node";
+
+/**
+ * Merges two sorted linked lists into a single sorted linked list.
+ * The merged list is formed by splicing together the nodes of the input lists.
+ *
+ * @param {ListNode | null} list1 - The head of the first sorted linked list.
+ * @param {ListNode | null} list2 - The head of the second sorted linked list.
+ * @returns {ListNode | null} The head of the merged sorted linked list.
+ *
+ * @example
+ * // Input: list1 = [1, 2, 4], list2 = [1, 3, 4]
+ * // Output: [1, 1, 2, 3, 4, 4]
+ *
+ * @example
+ * // Input: list1 = [], list2 = []
+ * // Output: []
+ *
+ * @example
+ * // Input: list1 = [], list2 = [0]
+ * // Output: [0]
+ */
+export function mergeTwoLists(
+	list1: ListNode | null,
+	list2: ListNode | null,
+): ListNode | null {
+	return null;
+}
---
+++ src/linked-list/merge-two-srted-lists.spec.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it } from "bun:test";
+import { mergeTwoLists } from "./merge-two-sorted-lists";
+import { arrayToLinkedList } from "./utils/array-to-linked-list";
+import { linkedListToArray } from "./utils/linked-list-to-array";
+
+describe("mergeTwoLists", () => {
+	it("should merge two sorted linked lists", () => {
+		const list1 = arrayToLinkedList([1, 2, 4]);
+		const list2 = arrayToLinkedList([1, 3, 4]);
+		const merged = mergeTwoLists(list1, list2);
+
+		expect(linkedListToArray(merged)).toEqual([1, 1, 2, 3, 4, 4]);
+	});
+
+	it("should return an empty list when both lists are empty", () => {
+		const list1 = arrayToLinkedList([]);
+		const list2 = arrayToLinkedList([]);
+		const merged = mergeTwoLists(list1, list2);
+
+		expect(merged).toBeNull();
+	});
+
+	it("should return the non-empty list when one list is empty", () => {
+		const list1 = arrayToLinkedList([]);
+		const list2 = arrayToLinkedList([0]);
+		const merged = mergeTwoLists(list1, list2);
+
+		expect(linkedListToArray(merged)).toEqual([0]);
+	});
+
+	it("should correctly merge when all elements in one list are smaller", () => {
+		const list1 = arrayToLinkedList([1, 2, 3]);
+		const list2 = arrayToLinkedList([4, 5, 6]);
+		const merged = mergeTwoLists(list1, list2);
+
+		expect(linkedListToArray(merged)).toEqual([1, 2, 3, 4, 5, 6]);
+	});
+
+	it("should correctly merge when lists have interleaved values", () => {
+		const list1 = arrayToLinkedList([1, 3, 5]);
+		const list2 = arrayToLinkedList([2, 4, 6]);
+		const merged = mergeTwoLists(list1, list2);
+
+		expect(linkedListToArray(merged)).toEqual([1, 2, 3, 4, 5, 6]);
+	});
+});
